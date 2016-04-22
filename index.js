38738d5f0bc367a0ab62ba1a1d4df57015fb3b56
Adrián Gómez <g.adrian@elaniin.com>
2016-04-21 18:12:13 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+var port = process.env.PORT || 8080;
+app.use(bodyParser.json());
+
+//Your FanPageToken Generated in your FB App
+var token = "<page_access_token>";
+var verify_token = "<validation_token>";
+
+//Root EndPoint
+app.get('/', function (req, res) {
+
+    res.send('Facebook Messenger Bot root endpoint!');
+
+});
+
+//Setup Webhook
+app.get('/webhook/', function (req, res) {
+
+    if (req.query['hub.verify_token'] === verify_token) {
+        res.send(req.query['hub.challenge']);
+    }
+
+    res.send('Error, wrong validation token');
+
+});
+
+app.post('/webhook/', function (req, res) {
+
+    var messaging_events = req.body.entry[0].messaging;
+
+    for (var i = 0; i < messaging_events.length; i++) {
+
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+
+            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+        }
+    }
+
+    res.sendStatus(200);
+
+});
+
+//App listen
+app.listen(port, function () {
+
+    console.log('Facebook Messenger Bot on port: ' + port);
+
+});
+
+//send Message with Facebook Graph Facebook v2.6
+function sendTextMessage(sender, text) {
+
+    var messageData = {
+        text: text
+    };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response) {
+
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+
+    });
+
+}
